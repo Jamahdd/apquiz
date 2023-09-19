e236a503796d21540de9409989b232eff12ec945
Jamahdd <144923650+Jamahdd@users.noreply.github.com>
2023-09-19 15:23:37 +0000

    activit work

---
+++ apPythonActivities/builtInFunctions.py
@@ -0,0 +1,12 @@
+#type() = prints the data type wrapped in the round brackets
+ 
+ type_of_Car = 'lambo
+ 
+car = type(type_of_Car)
+
+print(car)
+
+#use the type function to print out the data tye for each line of code.
+is_it_a_week_day = True
+
+number_of_books = 10
