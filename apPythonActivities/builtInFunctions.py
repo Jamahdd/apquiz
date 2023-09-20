00cc966fb0a28a0591eb35a671c1cd7f55b85623
Jamahdd <144923650+Jamahdd@users.noreply.github.com>
2023-09-20 15:24:34 +0000

    classwork

--- apPythonActivities/builtInFunctions.py
+++ apPythonActivities/builtInFunctions.py
@@ -10,3 +10,32 @@ print(car)
 is_it_a_week_day = True
 
 number_of_books = 10
+
+# convert the following datatypes into floats
+     12 
+     100
+
+     print(float(12))
+     print(float(100))
+
+# convert the following floats into a integers
+    12.34
+    23.33
+
+    print(int(12.34))
+    print(int(23.33))
+
+    
+
+#modify the code below t print an integer number
+num_one = 20
+num_two= 23.45
+
+print(num_one + num_two)
+
+print(int(num_one + num_two))
+#modify the following code below to 
+name_of_pet = "spuny"
+age_of_pet = 10
+
+print("this is my dog"+ name_of_pet+". He is" + age_of_pet+ "years old.")
